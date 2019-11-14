2c08083fea25be1790b05be248959133444b26f7
Philippe Lambet <philippe.lambert.fr@gmail.com>
2019-11-14 15:34:49 +0100

    first commit

---
+++ src/components/Counter.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+function Counter() {
+  // Déclare une nouvelle variable d'état, que l'on va appeler « count »
+  const [count, setCount] = useState(10);
+  //const [start, setStart] = useState(0);
+
+  return (
+    <div>
+      <p>Vous avez cliqué {count} fois</p>
+      <button onClick={() => setCount(count + 1)}>+1</button>
+      <button onClick={() => setCount(count - 1)}>-1</button>
+    </div>
+  );
+}
+
+export default Counter;
