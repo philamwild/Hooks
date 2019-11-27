6b648a505ea61c5f4d9e5524f170e156249cd506
philamwild <55431067+philamwild@users.noreply.github.com>
2019-11-27 15:14:47 +0100

    Update Counter.jsx

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,12 +2,17 @@ import React, { useState } from "react";
 
 function Counter() {
   // Déclare une nouvelle variable d'état, que l'on va appeler « count »
-  const [count, setCount] = useState(10);
-  //const [start, setStart] = useState(0);
+  const [count, setCount] = useState(0);
 
   return (
     <div>
-      <input value={count} type="number" />
+      <input
+        value={count}
+        type="number"
+        onChange={event => {
+          setCount(parseInt(event.target.value));
+        }}
+      />
       <p>Vous avez cliqué {count} fois</p>
       <button onClick={() => setCount(count + 1)}>+1</button>
       <button onClick={() => setCount(count - 1)}>-1</button>
